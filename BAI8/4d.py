3b848e22cd3e623a81c367d40fc63fcf1aa42317
nguyenphuochung2002 <nguyenphuochung642002@gmail.com>
2024-12-18 06:54:36 +0700

    Add files via upload

---
+++ BAI8/4d.py
@@ -0,0 +1,13 @@
+print("sinh viên : Nguyen Phuoc Hung 205751030110042")
+###########################
+from tkinter import *
+window = Tk()
+window.title("welcome to likegeeks app")
+window.geometry('350x200')
+lbl = Label(window,text="hello")
+lbl.grid(column=0,row=0)
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+btn = Button(window, text="Click Me",bg='red',fg='yellow', command=clicked)
+btn.grid(column=1, row=0)
+window.mainloop()
